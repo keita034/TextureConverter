37616adc3060c167a55ad6bf581b529e553622db
keita_momiyama <93181795+keita034@users.noreply.github.com>
2023-10-01 09:32:11 +0900

    02_05

--- TextureConverter.h
+++ TextureConverter.h
@@ -6,7 +6,16 @@ class TextureConverter
 {
 private:
 
-
+	//画像の情報
+	DirectX::TexMetadata metadata_;
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
 public:
 
 	/// <summary>
@@ -32,5 +41,16 @@ private:
 	/// <param name="mString">マルチバイト文字列</param>
 	/// <returns>ワイド文字列</returns>
 	static std::wstring ConvertMultiByteStrigToWideString(const std::string& mString);
+
+	/// <summary>
+	/// フォルダパスとファイル名を分離する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
+
+	/// <summary>
+	/// DDSテクスチャとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
 };
 
